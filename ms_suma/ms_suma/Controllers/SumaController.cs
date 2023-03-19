0b32972275eeb57aee0a44f23d0a3689231163c0
Natalia Gaona Salamanca <70587109+Nat-sourc@users.noreply.github.com>
2023-03-19 13:58:24 -0500

    Inicio proyecto

---
+++ ms_suma/ms_suma/Controllers/SumaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ms_suma.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SumaController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Sumar(SumandosDto sumandos)
+        {
+            sumandos.resultado=sumandos.sumando1+sumandos.sumando2;
+            return Ok(sumandos);
+        }
+    
+    }
+}
---
+++ ms_suma/ms_suma/Sumandos.cs
@@ -0,0 +1,9 @@
+﻿namespace ms_suma
+{
+    public class SumandosDto
+    {
+        public float sumando1 { get;set; }
+        public float sumando2 { get;set; }
+        public float resultado { get; set; }
+    }
+}
